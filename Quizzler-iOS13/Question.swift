4e51b755a81ca0b755fe8dff5e2dca14705eebfb
Famil <frostgamingrespawn@gmail.com>
2020-09-20 22:31:06 +0200

    Questions and struct added

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Famil Samadli on 9/20/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+}
--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -15,13 +15,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
+    let quiz = [
+        Question(text: "A slug's blood is green.", answer: "True"),
+        Question(text: "Approximately one quarter of human bones are in the feet.", answer: "True"),
+        Question(text: "The total surface area of two human lungs is approximately 70 square metres.", answer: "True"),
+        Question(text: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", answer: "True"),
+        Question(text: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", answer: "False"),
+        Question(text: "It is illegal to pee in the Ocean in Portugal.", answer: "True"),
+        Question(text: "You can lead a cow down stairs but not up stairs.", answer: "False"),
+        Question(text: "Google was originally called 'Backrub'.", answer: "True"),
+        Question(text: "Buzz Aldrin's mother's maiden name was 'Moon'.", answer: "True"),
+        Question(text: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", answer: "False"),
+        Question(text: "No piece of square dry paper can be folded in half more than 7 times.", answer: "False"),
+        Question(text: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", answer: "True")
+    ];
+    
+    var questionNumber = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateUI();
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
         
+        let userAnswer = sender.currentTitle!;
+        let actualAnswer = quiz[questionNumber].answer;
+        
+        questionNumber+=1;
+        updateUI();
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber].text;
     }
     
 }
