df85e40ac54342f8316f0e8ef1590447ee290cbc
Famil <frostgamingrespawn@gmail.com>
2020-09-20 23:48:29 +0200

    MVC refactor and score counter added

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -10,31 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
-    let quiz = [
-        Question(text: "A slug's blood is green.", answer: "True"),
-        Question(text: "Approximately one quarter of human bones are in the feet.", answer: "True"),
-        Question(text: "The total surface area of two human lungs is approximately 70 square metres.", answer: "True"),
-        Question(text: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", answer: "True"),
-        Question(text: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", answer: "False"),
-        Question(text: "It is illegal to pee in the Ocean in Portugal.", answer: "True"),
-        Question(text: "You can lead a cow down stairs but not up stairs.", answer: "False"),
-        Question(text: "Google was originally called 'Backrub'.", answer: "True"),
-        Question(text: "Buzz Aldrin's mother's maiden name was 'Moon'.", answer: "True"),
-        Question(text: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", answer: "False"),
-        Question(text: "No piece of square dry paper can be folded in half more than 7 times.", answer: "False"),
-        Question(text: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", answer: "True")
-    ];
+    var quizBrain = QuizBrain();
     
-    var questionNumber = 0;
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        progressBar.progress = 0;
+        super.viewDidLoad();
         
         updateUI();
     }
@@ -42,28 +28,23 @@ class ViewController: UIViewController {
     @IBAction func buttonPressed(_ sender: UIButton) {
         
         let userAnswer = sender.currentTitle!;
-        let actualAnswer = quiz[questionNumber].answer;
+        let userGotItRight = quizBrain.checkAnswer(userAnswer);
         
-        if userAnswer == actualAnswer {
+        if (userGotItRight) {
             sender.backgroundColor = UIColor.green;
         }else {
             sender.backgroundColor = UIColor.red;
         }
         
-        if questionNumber+1 < quiz.count {
-            questionNumber+=1;
-            progressBar.progress = Float(questionNumber + 1) / Float(quiz.count);
-        } else {
-            questionNumber=0;
-            progressBar.progress = 0;
-        }
+        quizBrain.nextQuestion();
         
         Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: true);
     }
     
     @objc func updateUI() {
-        questionLabel.text = quiz[questionNumber].text;
-        
+        questionLabel.text = quizBrain.getQuestionText();
+        progressBar.progress = quizBrain.getProgress();
+        scoreLabel.text = "Score: \(quizBrain.getScore())";
         trueButton.backgroundColor = UIColor.clear;
         falseButton.backgroundColor = UIColor.clear;
     }
